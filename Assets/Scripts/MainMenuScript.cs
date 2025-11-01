9f32fb4f326be2c8f285ebc55f21e36793680c39
M-A-R-M <mauricio.reyna@saei.mx>
2025-11-01 10:38:28 -0600

    MenuFunctions
    Main Menu fixed

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;  
 
 public class MainMenuScript : MonoBehaviour
 {
+    public Image fadePanel;
+    public float fadeDuration = 1f;
+
+    private void Start()
+    {
+        fadePanel.color = new Color(0, 0, 0, 1);
+        fadePanel.DOFade(0f, fadeDuration).OnComplete(() =>
+        {
+            fadePanel.raycastTarget = false;
+        });
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        fadePanel.raycastTarget = true;
+
+        fadePanel.DOFade(1f, fadeDuration).OnComplete(() =>
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        });
     }
 
     public void QuitGame()
     {
         Debug.Log("Saliendo del juego");
-        Application.Quit();
+        Application.Quit(); 
     }
 }
